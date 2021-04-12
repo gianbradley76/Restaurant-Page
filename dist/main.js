a33cd0bf965d05f16e9566b462cb8fd1cac7bf68
Gian Bradley Mendoza <gbpmendoza76@gmail.com>
2021-04-12 15:50:30 +0800

    Redesigned and finalized home page

--- dist/main.js
+++ dist/main.js
@@ -1,55 +1,5 @@
 /******/ (() => { // webpackBootstrap
 var __webpack_exports__ = {};
-console.log("asdasdad");
-
-const carouselSlide = document.querySelector(".carousel-slide");
-const carouselImages = document.querySelectorAll(".carousel-slide img");
-
-const prevBtn = document.querySelector("#prev-btn");
-const nextBtn = document.querySelector("#next-btn");
-
-let counter = 1;
-const size = carouselImages[0].clientWidth;
-
-carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-
-nextBtn.addEventListener("click", () => {
-	if (counter >= carouselImages.length - 1) {
-		return;
-	}
-	carouselSlide.style.transition = "transform 0.4s ease-in-out";
-	counter++;
-	carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-});
-
-prevBtn.addEventListener("click", () => {
-	if (counter <= 0) {
-		return;
-	}
-	carouselSlide.style.transition = "transform 0.4s ease-in-out";
-	counter--;
-	carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-});
-
-carouselSlide.addEventListener("transitionend", () => {
-	if (carouselImages[counter].id === "last-clone") {
-		carouselSlide.style.transition = "none";
-		counter = carouselImages.length - 2;
-		carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-	}
-	if (carouselImages[counter].id === "first-clone") {
-		carouselSlide.style.transition = "none";
-		counter = carouselImages.length - counter;
-		carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-	}
-});
-/* 
-setInterval(() => {
-	carouselSlide.style.transition = "transform 0.4s ease-in-out";
-	counter++;
-	carouselSlide.style.transform = "translateX(" + -size * counter + "px)";
-}, 10000);
- */
 
 /******/ })()
 ;
