34b8d7088f585ba7dbdac3038d5a1d17dc9acd61
Gian Bradley Mendoza <gbpmendoza76@gmail.com>
2021-04-13 17:18:20 +0800

    transferred home page from HTML to JS module

--- main.js
+++ main.js
@@ -1,5 +1,182 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ([
+/* 0 */,
+/* 1 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+function creatNav() {
+	const nav = document.createElement("nav");
+
+	const logoContainer = document.createElement("div");
+	logoContainer.classList.add("logo");
+
+	const logo = document.createElement("h4");
+	logo.innerText = "The Coffee Shop";
+	logoContainer.appendChild(logo);
+
+	const navLinks = document.createElement("ul");
+	navLinks.classList.add("nav-links");
+
+	const links = ["Home", "Menu", "Contact Us"];
+
+	links.forEach((link) => {
+		const li = document.createElement("li");
+
+		const anchor = document.createElement("A");
+		anchor.href = "#";
+		anchor.textContent = link;
+
+		li.appendChild(anchor);
+
+		navLinks.appendChild(li);
+	});
+
+	nav.appendChild(logoContainer);
+	nav.appendChild(navLinks);
+
+	return nav;
+}
+
+function createMain() {
+	const main = document.createElement("main");
+	main.classList.add("home-content");
+
+	const mainBG = document.createElement("img");
+	mainBG.srcset = "./media/home-background.png";
+
+	const homeHeaders = document.createElement("div");
+	homeHeaders.classList.add("home-headers");
+
+	const mainHeader = document.createElement("h1");
+	mainHeader.innerText = "MAIN HEADER";
+
+	const subHeader = document.createElement("h2");
+	subHeader.innerHTML = "Sub Header";
+
+	homeHeaders.appendChild(mainHeader);
+	homeHeaders.appendChild(subHeader);
+
+	const orderBtn = document.createElement("A");
+	orderBtn.href = "#";
+	orderBtn.classList.add("order-btn");
+	orderBtn.innerHTML = "Order Here";
+
+	main.appendChild(mainBG);
+	main.appendChild(homeHeaders);
+	main.appendChild(orderBtn);
+
+	return main;
+}
+
+function createFooter() {
+	/* 
+	<footer>
+		<p>Something Something Something Something Something Something</p>
+		<p>Something Something Something Something Something</p>
+		<p>Something Something Something Something</p>
+	</footer>
+	*/
+	const footer = document.createElement("footer");
+
+	const item1 = document.createElement("p");
+	item1.innerText =
+		"Something Something Something Something Something Something";
+
+	const item2 = document.createElement("p");
+	item2.innerText = "Something Something Something Something Something";
+
+	const item3 = document.createElement("p");
+	item3.innerText = "Something Something Something Something";
+
+	footer.appendChild(item1);
+	footer.appendChild(item2);
+	footer.appendChild(item3);
+
+	return footer;
+}
+
+function displayWebsite() {
+	const content = document.querySelector(".content");
+	content.appendChild(creatNav());
+	content.appendChild(createMain());
+	content.appendChild(createFooter());
+}
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (displayWebsite);
+
+
+/***/ })
+/******/ 	]);
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _home_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+
+
+(0,_home_js__WEBPACK_IMPORTED_MODULE_0__.default)();
+
+})();
 
 /******/ })()
 ;
